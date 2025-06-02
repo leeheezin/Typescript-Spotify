ded07b6b0655e484bcc6377954775bf38252ebb8
heeezin <gmlwls839@nate.com>
2025-06-02 15:02:20 +0900

    재설치

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router';
 import Loading from './common/components/Loading/Loading';
+
 const AppLayout = React.lazy(()=> import('./layout/AppLayout'))
 const HomePage = React.lazy(()=>import('./pages/HomePage/Homepage'))
 const SearchPage = React.lazy(()=>import('./pages/SearchPage/SearchPage'))
