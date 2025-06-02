1d1f90859d7966faf4ea8474ebc9ba3eac751e9e
heeezin <gmlwls839@nate.com>
2025-06-02 14:32:20 +0900

    웹팩바벨라우터세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Suspense } from 'react';
 import './App.css';
-
+import { Route, Routes } from 'react-router';
+import Loading from './common/components/Loading/Loading';
+const AppLayout = React.lazy(()=> import('./layout/AppLayout'))
+const HomePage = React.lazy(()=>import('./pages/HomePage/Homepage'))
+const SearchPage = React.lazy(()=>import('./pages/SearchPage/SearchPage'))
+const SearchWithKeywordPage = React.lazy(()=>import('./pages/SearchPage/SearchWithKeywordPage'))
+const PlayListPage = React.lazy(()=>import('./pages/PlayListPage/PlayListPage'))
+//0. sidebar(playlist,menu) 1. home / 2. search /search 3. search result /search/:keyword 4. play detail /playlist/:id 5. (mobile)playlist view /playlist
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<Loading/>}>
+      <Routes>
+        <Route path="/" element={<AppLayout/>}>
+          <Route index element={<HomePage/>}/>
+          <Route path="search" element={<SearchPage/>}/>
+          <Route path="search/:keyword" element={<SearchWithKeywordPage/>}/>
+          <Route path="playlist/:id" element={<PlayListPage/>}/>
+          {/* <Route path="/playlist" element={<LibraryPage/>}/> */}
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
